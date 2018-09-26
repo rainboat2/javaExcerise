932dab64908140314559b79c39150d23c82d7e52
rainboat2 <32905130+rainboat2@users.noreply.github.com>
2018-09-26 17:53:56 +0800

    temp

---
+++ src/excerise/MyLinkedList.java
@@ -0,0 +1,425 @@
+package excerise;
+
+import java.util.*;
+
+public class MyLinkedList<Type> implements List<Type>, Cloneable {
+
+    private Node<Type> head;
+    private Node<Type> tail;
+    private int size;
+    private int modTimes;    //记录被修改的次数
+
+
+    /**
+     * head只需要next， 而tail只需要pre，故两者可以共用一个tail类
+     * head和tail尽管是两个不同的引用，但由于其指向一个对象，两者的值是一样的
+     * 本链表的本质是一个循环链表
+     */
+    public MyLinkedList(){
+        clear();
+    }
+
+    private class Node<Type>{
+        Type element;
+        Node<Type> next;
+        Node<Type> pre;
+
+        public Node(){}
+        public Node(Type element, Node<Type> next, Node<Type> pre){
+            this.element = element;
+            this.next = next;
+            this.pre = pre;
+        }
+
+        public String toString(){
+            return element+"";
+        }
+    }
+
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (Node<Type> temp = head.next; temp != tail; temp = temp.next){
+            if (temp.equals(o))  return true;
+        }
+        return false;
+    }
+
+    private class MyIterator implements Iterator<Type>{
+
+        private Node<Type> current = head;
+        private int mod = modTimes;
+        private int index;
+
+        @Override
+        public boolean hasNext() {
+            if (modTimes != mod)
+                throw new ConcurrentModificationException();
+            return index < size;
+        }
+
+        @Override
+        public Type next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            index++;
+            Type value = current.element;
+            current = current.next;
+            return value;
+        }
+    }
+
+    @Override
+    public Iterator<Type> iterator() {
+        return new MyIterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+        Node<Type> temp = head;
+        for (int i = 0; i < size; i++){
+            temp = temp.next;
+            array[i] = temp.element;
+        }
+        return array;
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        a = (T[]) toArray();
+        return a;
+    }
+
+    @Override
+    public boolean add(Type type) {
+        add(size, type);
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        for (Node<Type> temp = head.next; temp.next != tail; temp = temp.next){
+            if (temp.element.equals(o)){
+                temp.pre.next = temp.next;
+                temp.next.pre = temp.pre;
+                size--;
+                modTimes++;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object temp : c){
+            if (!contains(temp))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+        for (int i = 0; i < 39; i++) list.add(i);
+
+        MyLinkedList<Integer> temp = new MyLinkedList<>();
+        for (int i =90; i < 130; i++) temp.add(i);
+
+        list.addAll(temp);
+        System.out.println(list);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Type> c) {
+        return addAll(size, c);
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends Type> c) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException();
+
+        //找到插入位置前面和后面的两个节点
+        Node<Type> after = getNode(index);
+        Node<Type> before = after.pre;
+
+        //构造一个双向链表，方便后面插入
+        Node<Type> head = new Node<>();
+        Node<Type> tail = new Node<>();
+        head.next = tail;
+        tail.pre = head;
+        Iterator it = c.iterator();
+        while (it.hasNext()){
+            Node<Type> newNode = new Node<>((Type)it.next(), tail, tail.pre);
+            tail.pre = newNode;
+            //TODO:构造链表失败，新的元素没能成功添加
+        }
+
+        //开始插入整个链表
+        before.next = head.next;
+        head.next.pre = before;
+
+        tail.pre.next = after;
+        after.pre = tail.pre;
+        size += c.size();
+        modTimes++;
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean flag = false;
+        Iterator it = c.iterator();
+        while (it.hasNext()){
+            flag = remove(it.next()) || flag;
+        }
+        return flag;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean flag = false;
+        for (Node<Type> temp = head.next; temp != tail; temp = temp.next){
+            if (!c.contains(temp)){
+                temp.pre.next = temp.next;
+                temp.next.pre = temp.pre;
+                flag = true;
+                size--;
+                modTimes++;
+            }
+        }
+        return flag;
+    }
+
+    @Override
+    public void clear() {
+        head = new Node<>();
+        tail = head;
+        head.next = tail;
+        tail.pre = head;
+        size = 0;
+    }
+
+    private Node<Type> getNode(int index){
+        //当index在靠前的位置时，从前往后遍历，靠后时，则从后往前遍历
+        Node<Type> temp;
+        if ((size/2) > index){
+            temp = head;
+            for(int i = -1; i < index; i++)
+                temp = temp.next;
+        }else{
+            temp = tail;
+            for(int i = size; i > index; i--)
+                temp = temp.pre;
+        }
+        return temp;
+    }
+
+    @Override
+    public Type get(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+        return this.getNode(index).element;
+    }
+
+    @Override
+    public Type set(int index, Type element) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+
+        Node<Type> temp = getNode(index);
+        Type elem = temp.element;
+        temp.element = element;
+        modTimes++;
+        return elem;
+    }
+
+    @Override
+    public void add(int index, Type element) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException();
+
+        Node<Type> temp = getNode(index);
+        Node<Type> newNode = new Node<>();
+        newNode.element = element;
+        //开始将新的节点放在temp的前面
+        newNode.pre = temp.pre;
+        temp.pre.next = newNode;
+        temp.pre = newNode;
+        newNode.next = temp;
+        size++;
+        modTimes++;
+    }
+
+    @Override
+    public Type remove(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+
+        Node<Type> temp = getNode(index-1);   //获得要被删除的节点的前一个节点
+        Type elem = temp.next.element;
+
+        //开始删除指定节点
+        temp.next = temp.next.next;
+        temp.next.pre = temp;
+        size--;
+        modTimes++;
+        return elem;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        Node<Type> temp = head;
+        for (int i = 0; i < size; i++){
+            temp = temp.next;
+            if (temp.element.equals(0)){
+                return i;
+            }
+        }
+        return -2;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        Node<Type> temp = tail;
+        for (int i = size-1; i >= 0; i++){
+            temp = temp.pre;
+            if (temp.element.equals(o)){
+                return i;
+            }
+        }
+        return -2;
+    }
+
+    private class MyListIterator implements ListIterator<Type>{
+
+        private Node<Type> current;
+        private int mod = MyLinkedList.this.modTimes;
+        private int index;
+
+        public MyListIterator() {current = head; }
+
+        public MyListIterator(int index) {
+            if (index < 0 || index >= size)
+                throw new IndexOutOfBoundsException();
+            current = getNode(index);
+            this.index = index;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (modTimes != mod)
+                throw new ConcurrentModificationException();
+            return index != size;
+        }
+
+        @Override
+        public Type next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            Type value = current.element;
+            current = current.next;
+            index++;
+            return value;
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            if (modTimes != mod)
+                throw new ConcurrentModificationException();
+            return index > 0;
+        }
+
+        @Override
+        public Type previous() {
+            if (!hasPrevious())
+                throw new NoSuchElementException();
+            Type value = current.pre.element;
+            current = current.pre;
+            index--;
+            return value;
+        }
+
+        @Override
+        public int nextIndex() {
+            return index;
+        }
+
+        @Override
+        public int previousIndex() {
+            return index - 1;
+        }
+
+        @Override
+        public void remove() {
+            current = current.pre;
+            MyLinkedList.this.remove(this.index--);
+        }
+
+        @Override
+        public void set(Type type) {
+            if (modTimes != mod)
+                throw new ConcurrentModificationException();
+            current.element = type;
+        }
+
+        @Override
+        public void add(Type type) {
+            if (modTimes != mod)
+                throw new ConcurrentModificationException();
+            MyLinkedList.this.add(index, type);
+        }
+    }
+
+    @Override
+    public ListIterator<Type> listIterator() {
+        return new MyListIterator();
+    }
+
+    @Override
+    public ListIterator<Type> listIterator(int index) {
+        return new MyListIterator(index);
+    }
+
+    @Override
+    public List<Type> subList(int fromIndex, int toIndex) {
+        MyLinkedList<Type> list = new MyLinkedList<>();
+        Node<Type> temp = getNode(fromIndex);
+        for (int i = 0; i < toIndex; i++){
+            list.add(temp.element);
+            temp = temp.next;
+        }
+        return list;
+    }
+
+    public String toString(){
+        String s = "";
+        for (Node<Type> temp = head.next; temp != tail; temp = temp.next){
+            s += temp.element + " ";
+        }
+        return s;
+    }
+
+    public MyLinkedList<Type> clone(){
+        try {
+            super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        MyLinkedList<Type> linkedList = new MyLinkedList<>();
+        linkedList.addAll(this);
+        return linkedList;
+    }
+}
--- src/excerise/SequenceTest.java
+++ src/excerise/SequenceTest.java
@@ -5,110 +5,99 @@ import java.io.*;
 
 public class SequenceTest {
 
-
-
     public static void main(String[] args){
         for(int i = 1; i <=3; i++){
-            //normalTest(new File("普通测试_" + i + ".txt"));
+            normalTest(new File("普通测试_" + i + ".txt"));
         }
         bigDataTest(new File("bigDataTest.txt"));
     }
 
     public static void normalTest(File file){
-        SequenceList<Integer> list = new SequenceList<>();
+        MyLinkedList<Integer> list = new MyLinkedList<>();
         int num = (int)(Math.random()*100);
 
-        PrintWriter writer = null;
-        try {
-            FileOutputStream out = new FileOutputStream(file);
-            writer = new PrintWriter(out);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
 
-
-        writer.println("------添加测试-------");
-        writer.printf("任意添加%d个数\n", num);
+        System.out.println("------添加测试-------");
+        System.out.printf("任意添加%d个数\n", num);
         for(int i = 0; i < num; i++)
             list.add(i);
-        writer.printf("添加成功，目前长度为%d，数组为：%s\n", list.size(), list.toString());
+        System.out.printf("添加成功，目前长度为%d，数组为：%s\n", list.size(), list.toString());
 
-        writer.println("------删除测试-------");
-        writer.println("随机获得一个list的子串");
-        SequenceList<Integer> sub = (SequenceList<Integer>) list.subList(num/4, num/2);
-        writer.println("subList: " + sub);
-        writer.println("以sublist为参数，删除原list中的子串");
+        System.out.println("------删除测试-------");
+        System.out.println("随机获得一个list的子串");
+        MyLinkedList<Integer> sub = (MyLinkedList<Integer>) list.subList(num/4, num/2);
+        System.out.println("subList: " + sub);
+        System.out.println("以sublist为参数，删除原list中的子串");
         list.removeAll(sub);
-        writer.printf("删除成功，目前长度为%d，数组为：%s\n", list.size(), list.toString());
+        System.out.printf("删除成功，目前长度为%d，数组为：%s\n", list.size(), list.toString());
 
-        writer.println("------清除测试-------");
-        writer.println("开始清除");
+        System.out.println("------清除测试-------");
+        System.out.println("开始清除");
         list.clear();
-        writer.printf("清除成功，是否为空：%b\n", list.isEmpty());
+        System.out.printf("清除成功，是否为空：%b\n", list.isEmpty());
 
-        writer.println("------插入测试-------");
+        System.out.println("------插入测试-------");
         num = (int)(Math.random()*30) + 5;
-        writer.printf("任意插入%d个数\n", num);
+        System.out.printf("任意插入%d个数\n", num);
         for(int i = 0; i < num; i++){
             int index = (int)(Math.random()*list.size());
             list.add(index, i);
         }
-        writer.printf("插入成功，目前长度为%d，数组为：%s\n", list.size(), list.toString());
+        System.out.printf("插入成功，目前长度为%d，数组为：%s\n", list.size(), list.toString());
 
-        writer.println("------get&set------");
-        writer.println("测试前：" + list);
+        System.out.println("------get&set------");
+        System.out.println("测试前：" + list);
         num = list.size()/2;
-        writer.printf("测试将list中所有大于%s的数变成零\n", num);
+        System.out.printf("测试将list中所有大于%s的数变成零\n", num);
         for(int i = 0; i < list.size(); i++){
             if(list.get(i) > num) list.set(i, 0);
         }
-        writer.println("改变成功，现在数组为" + list);
+        System.out.println("改变成功，现在数组为" + list);
 
-        writer.println("------查找测试------");
-        writer.println("找到并删除所有为0的项");
+        System.out.println("------查找测试------");
+        System.out.println("找到并删除所有为0的项");
         while (true){
             int index = list.indexOf(0);
-            if (index != -1)  list.remove(index);
+            if (index != -2)  list.remove(index);
             else              break;
         }
-        writer.println("测试成功，数组变为：" + list);
+        System.out.println("测试成功，数组变为：" + list);
 
-        writer.println("------拷贝测试------");
-        writer.println("将数组拷贝一份并添加到原数组的后面");
-        SequenceList<Integer> temp = list.clone();
+        System.out.println("------拷贝测试------");
+        System.out.println("将数组拷贝一份并添加到原数组的后面");
+        MyLinkedList<Integer> temp = list.clone();
+        System.out.println(temp);
         list.addAll(temp);
-        writer.println("测试成功，数组为：" + list);
+        System.out.println("测试成功，数组为：" + list);
 
-        writer.println("-----迭代器测试------");
-        writer.println("使用listIterator来对SequenceList进行倒序遍历并打印");
+        System.out.println("-----迭代器测试------");
+        System.out.println("使用listIterator来对SequenceList进行倒序遍历并打印");
         ListIterator<Integer> lit = list.listIterator(list.size());
         while(lit.hasPrevious()){
-            writer.print(lit.previous() + "  ");
+            System.out.print(lit.previous() + "  ");
         }
-        writer.println();
-        writer.println("使用listIterator来对SequenceList进行遍历并打印");
+        System.out.println();
+        System.out.println("使用listIterator来对SequenceList进行遍历并打印");
         lit = list.listIterator();
         while (lit.hasNext()){
-            writer.print(lit.next() + "  ");
+            System.out.print(lit.next() + "  ");
         }
-        writer.println();
-        writer.println("“使用Iterator来对sequenceList进行遍历并打印");
+        System.out.println();
+        System.out.println("“使用Iterator来对sequenceList进行遍历并打印");
         Iterator<Integer> it = list.listIterator();
         while (it.hasNext()){
-            writer.print(it.next() + "  ");
+            System.out.print(it.next() + "  ");
         }
-        writer.flush();
-        writer.close();
     }
 
     public static void bigDataTest(File file) {
-        SequenceList<String> list = new SequenceList<>();
+        MyLinkedList<String> list = new MyLinkedList<>();
         PrintWriter writer = null;
         BufferedReader reader = null;
         try {
             FileReader in = new FileReader(new File("bigData.txt"));
             FileOutputStream out = new FileOutputStream(file);
-            writer = new PrintWriter(out);
+            writer  = new PrintWriter(out);
             reader = new BufferedReader(in);
             String line = null;
             while((line = reader.readLine()) != null){
@@ -120,15 +109,15 @@ public class SequenceTest {
         }catch (IOException e){
             e.printStackTrace();
         }
-        writer.println("将文本文件中的单词取出并放在list中，统计单词数量");
+        System.out.println("将文本文件中的单词取出并放在list中，统计单词数量");
         boolean flag = true;
         while(flag){
             flag = list.remove("");          //删除标点符号与空格之间的空白字符（无字符）
         }
         //删除所有的数字
         list.removeAll(Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"));
-        writer.printf("单词数量为%s\n", list.size());
-        writer.flush();
-        writer.close();
+        System.out.printf("单词数量为%s\n", list.size());
+        System.out.flush();
+        System.out.close();
     }
 }
