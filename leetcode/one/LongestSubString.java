42e05cce9a585d631e268257f3fe8ec89e5117b4
rainboat2 <32905130+rainboat2@users.noreply.github.com>
2018-10-06 22:53:18 +0800

    some exerise

--- leetcode/one/LongestSubString.java
+++ leetcode/one/LongestSubString.java
@@ -1,18 +1,72 @@
 package one;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class LongestSubString {
 
     public static void main(String[] args){
         LongestSubString l = new LongestSubString();
-        String s = "abcabcbb";
-        int a = l.lengthOfLongestSubstring(s);
-        System.out.println(a);
-        System.out.println(s.substring(0,1));
+        String s1 = "abcabcdef";
+        String s2 = "bbbbbb";
+        System.out.println(l.lengthOfLongestSubstring(s2));
     }
 
     public int lengthOfLongestSubstring(String s) {
         int maxLength = 0;
         int begin = 0;
-        return -1;
+        int end;
+        for (end = 1; end < s.length(); end++){
+            char c = s.charAt(end);
+            //寻找目前的子串中是否有和c重复的字符
+            for (int i = begin; i < end; i++){
+                if (s.charAt(i) == c){
+                    maxLength = Math.max(maxLength, end - begin);
+                    begin = i + 1;
+                    break;
+                }
+            }
+        }
+        if (s.length() - begin > maxLength)  maxLength = s.length() - begin;
+        return maxLength;
+    }
+
+    /**
+     * @param s
+     * @return int
+     *
+     * 更好的方法，来源leetcode解答
+     */
+    public int lengthOfLongestSubstring1(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
     }
 }
---
+++ leetcode/one/MedianOfTwoSortedArray.java
@@ -0,0 +1,17 @@
+package one;
+
+public class MedianOfTwoSortedArray {
+
+    public static void main(String[] args){
+        MedianOfTwoSortedArray m = new MedianOfTwoSortedArray();
+        m.findMedianSortedArrays(new int[]{1}, new int[]{2});
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+    }
+
+    public int findK(int[] nums1, int i, int[] nums2, int j){
+
+    }
+}
