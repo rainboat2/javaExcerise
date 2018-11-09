8337eceb6969dfa0c1353600757a9ad8985e33f0
rainboat2 <32905130+rainboat2@users.noreply.github.com>
2018-11-09 14:18:20 +0800

    finish the printer simulation task

--- algorithms/PrinterSimulation/Fifo_2.java
+++ algorithms/PrinterSimulation/Fifo_2.java
@@ -8,13 +8,13 @@ import java.util.LinkedList;
 
 class TryTest{
     public static void main(String[] args){
-        Try t = new Try();
+        Fifo_2 t = new Fifo_2();
         t.simulator("myFile/Experiment 1/arbitrary.run", "myFile/Experiment 1/arbitrary.myOut", 2);
         t.simulator("myFile/Experiment 1/bigfirst.run", "myFile/Experiment 1/bigfirst.myOut", "Priority", 3);
     }
 }
 
-public class Try extends Simulator{
+public class Fifo_2 extends Simulator{
 
     private int current_time;
     private Printer[] printers;
@@ -34,8 +34,6 @@ public class Try extends Simulator{
 
     public void simulator(String workPath, String outPath, String pattern, int printerNumbers){
         init(workPath, outPath, pattern, printerNumbers);
-        if (pattern.equals("Priority"))  log_printer.println("Priority Simulator\n");
-        else                             log_printer.println("FIFO Simulator\n");
         while (!allWorkDone()){
             addCurrentTime();
             checkArrival();
@@ -43,39 +41,36 @@ public class Try extends Simulator{
         }
         String report = analyzeLatencyTimes();
         log_printer.println(report);
-        end();
+        endSimulator();
     }
 
-    private void runAllPrinters(){
-        for (Printer printer : printers)
-            printer.work();
-    }
+    private int getCurrentTime() { return current_time;}
+
+    private void addCurrentTime() { current_time++;}
 
     private void init(String workPath, String outPath, String pattern, int printerNumbers){
         // 重新开始记时
         current_time = 0;
         printers = new Printer[printerNumbers];
-        createPrinter(outPath);
+        createLogPrinter(outPath);
         for (int i = 0; i < printerNumbers; i++) printers[i] = new Printer(pattern, log_printer);
         super.workLoad.clear();
         loadWork(workPath);
+        if (pattern.equals("Priority"))  log_printer.println("Priority Simulator\n");
+        else                             log_printer.println("FIFO Simulator\n");
     }
 
-    private void end(){
+    private void endSimulator(){
         log_printer.close();
     }
 
-    private void createPrinter(String outPath){
+    private void createLogPrinter(String outPath){
         try{
             log_printer = new PrintWriter(new File(outPath));
         }catch (FileNotFoundException e){
             e.printStackTrace();
         }
     }
-
-    public int getCurrentTime() { return current_time;}
-
-    private void addCurrentTime() { current_time++;}
 
     private boolean allWorkDone(){
         boolean flag = workLoad.isEmpty();
@@ -95,6 +90,11 @@ public class Try extends Simulator{
         }
     }
 
+    private void runAllPrinters(){
+        for (Printer printer : printers)
+            printer.work();
+    }
+
     private void dispatch(Event job){
         Printer lessWorkPrinter = printers[0];
         for (int i = 1; i < printers.length; i++){
@@ -105,7 +105,7 @@ public class Try extends Simulator{
     }
 
     private String analyzeLatencyTimes(){
-        LinkedList<Integer> latencyTimes = getLatencyTimes();
+        LinkedList<Integer> latencyTimes = collectLatencyTimes();
         int total_jobs = latencyTimes.size();
         int aggregate_latency = 0;
         for (int time : latencyTimes)  aggregate_latency += time;
@@ -117,13 +117,23 @@ public class Try extends Simulator{
                 total_jobs, aggregate_latency, mean_latency);
     }
 
-    private LinkedList<Integer> getLatencyTimes(){
+    private LinkedList<Integer> collectLatencyTimes(){
         LinkedList<Integer> latencyTimes = new LinkedList<>();
         for (Printer printer : printers)
             latencyTimes.addAll(printer.getLatencyTimes());
         return latencyTimes;
     }
 
+    /**
+     *  Printer对外提供以下几个功能：
+     *  1. work，                让printer执行 “一秒” 的任务
+     *  2. addDispatchedWork，   为printer添加任务
+     *  3. collectLatencyTimes，     获得printer对每一次工作等待时间的统计信息
+     *  4. isFree                打印机当前是否空闲
+     *  5. getTotalWorkProcess   获得该打印机总的工作量
+     *
+     *  通过printer中构造函数参数WorkPattern 可以指定打印机的工作模式为 “先进先出” 或 “优先队列”
+     */
     private class Printer{
 
         private int progress_bar;
@@ -133,14 +143,15 @@ public class Try extends Simulator{
         private PrintWriter log_printer;
         private int totalWorkProgress;
 
-        public Printer(String workPattern, PrintWriter log_printer){
+        // 打印机需要一个log_printer来打印工作日志
+        Printer(String workPattern, PrintWriter log_printer){
             if (workPattern.equals("Priority"))  workflow = new MyPriorityQueue<>();
             else                                 workflow = new MyQueue<>();
             this.log_printer = log_printer;
             latencyTimes = new LinkedList<>();
         }
 
-        public void work(){
+        void work(){
             // 查看是否需要执行添加任务的操作
             if       (!hasJob())       tryToGetWork();
             else if  (isFinished())    moveToNextWork();
@@ -149,16 +160,18 @@ public class Try extends Simulator{
 
         }
 
-        public void addDispatchedJob(Event event){
+        void addDispatchedJob(Event event){
             workflow.add(event);
             int progress = event.getJob().getNumber_of_pages()*seconds_per_page;
             totalWorkProgress += progress;
         }
 
-        public  boolean               isFree()              { return !hasJob() && workflow.isEmpty();}
-        public  int                   getTotalWorkProcess() { return totalWorkProgress;}
-        public  LinkedList<Integer>   getLatencyTimes()     { return latencyTimes;}
-        private boolean               hasJob()              { return current_event != null;}
+        boolean               isFree()              { return !hasJob() && workflow.isEmpty();}
+        int                   getTotalWorkProcess() { return totalWorkProgress;}
+        LinkedList<Integer>   getLatencyTimes()     { return latencyTimes;}
+
+
+        private boolean       hasJob()              { return current_event != null;}
 
         private void doJob(){
             progress_bar++;
